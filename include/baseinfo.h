570331be0303aae554ba9042b7c37544988cf2c3
kevinWu777 <254337598@qq.com>
2023-08-29 18:50:22 +0800

    modify 'fopen' in stdio.h to std::filesystem to compatibile windows

--- include/baseinfo.h
+++ include/baseinfo.h
@@ -7,13 +7,4 @@ extern unsigned int sn_headLength;
 extern unsigned int sn_bodyLength;
 extern unsigned int sn_totalHeadLength;
 
-
-#ifdef _WIN32
-#define  PATH_SEPARATOR "\\"
-#define WIDE_PATH_SEPARATOR L"\\"
-#else
-#define  PATH_SEPARATOR "/"
-#define WIDE_PATH_SEPARATOR L"/"
-#endif
-
 #endif
