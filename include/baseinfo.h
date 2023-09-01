95da53cf6bf0716fdbe1de1f257add45bbe69917
kevin_pc <254337598@qq.com>
2023-09-01 22:00:00 +0800

    compatital windows

--- include/baseinfo.h
+++ include/baseinfo.h
@@ -7,4 +7,13 @@ extern unsigned int sn_headLength;
 extern unsigned int sn_bodyLength;
 extern unsigned int sn_totalHeadLength;
 
+
+#ifdef _WIN32
+#define PATH_SEPARATOR "\\"
+#define WIDE_PATH_SEPARATOR L"\\"
+#else
+#define PATH_SEPARATOR "/"
+#define WIDE_PATH_SEPARATOR L"/"
+#endif
+
 #endif
